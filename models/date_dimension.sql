8e10121fa002f74028589d117ac114b6b5269739
Rodrigue VODOUNON <bignonvodounon@gmail.com>
2025-12-16 22:11:28 +0000

    daily_weather model created

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -4,16 +4,8 @@ WITH CTE AS(
         DATE(TO_TIMESTAMP(STARTED_AT)) AS DATE_STARTED_AT,
         HOUR(TO_TIMESTAMP(STARTED_AT)) AS HOUR_STARTED_AT,
         DAYNAME(TO_TIMESTAMP(STARTED_AT)),
-        CASE
-            WHEN DAYNAME(TO_TIMESTAMP(STARTED_AT)) IN('SAT', 'SUN') THEN 'WEEKEND'
-            ELSE 'BUSINESSDAY'
-        END AS DAY_TYPE,
-        CASE
-            WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) IN(12, 1, 2) THEN 'WINTER'
-            WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) IN(3, 4, 5) THEN 'SPRING'
-            WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) IN(6, 7,8) THEN 'SUMMER'
-            ELSE 'AUTUMN'
-        END AS STATION_OF_YEAR
+        {{day_type('STARTED_AT')}} AS DAY_TYPE,
+        {{get_season('STARTED_AT')}} AS STATION_OF_YEAR
     from {{source('demo','bike')}}
     where STARTED_AT <>'started_at'
 )
