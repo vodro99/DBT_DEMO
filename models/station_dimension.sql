c48cd91d8b5d9475534b57c50e6cd0c7aa6dcc4f
Rodrigue VODOUNON <bignonvodounon@gmail.com>
2025-12-16 23:02:04 +0000

    station_dimension model created

---
+++ models/station_dimension.sql
@@ -0,0 +1,12 @@
+    WITH BIKE AS(
+        select 
+        distinct
+        start_statio_id as station_id,
+        start_station_name as station_name,
+        start_lat as station_lat,
+        start_lng as start_station_lng
+        from {{ source('demo', 'bike') }}
+        where RIDE_ID!='ride_id'
+    )
+
+    select * from BIKE
